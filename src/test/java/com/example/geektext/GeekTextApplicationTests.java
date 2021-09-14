8c2846b89bbd98a9715d6257be5a992478ed62f7
lroche823 <lroche823@gmail.com>
2021-09-14 19:18:39 -0400

    initial "hello world" program

---
+++ src/test/java/com/example/geektext/GeekTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geektext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
