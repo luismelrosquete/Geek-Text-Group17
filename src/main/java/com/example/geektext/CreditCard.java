9412d041fbe899570d5f22f0cab183105fda6792
lroche823 <89804525+lroche823@users.noreply.github.com>
2021-10-04 14:51:14 -0400

    initial credit card entity functionality
    added credit card entity, repository, and relationship to user. added a few calls to user controller despite not working perfectly (yet)

---
+++ src/main/java/com/example/geektext/CreditCard.java
@@ -0,0 +1,58 @@
+package com.example.geektext;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity     //tells hibernate to make a table out of this class
+public class CreditCard {
+    //VARS
+    @Id
+    private String cardNumber;
+    private int cardSecurityPin;
+    private int cardExpiryMonth;
+    private int cardExpiryYear;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "userName", nullable = false)
+    @JsonBackReference
+    private User user;
+
+    //GETTERS
+    public String getCardNumber()
+    {
+        return cardNumber;
+    }
+    public int getCardSecurityPin()
+    {
+        return cardSecurityPin;
+    }
+    public int getCardExpiryMonth()
+    {
+        return cardExpiryMonth;
+    }
+    public int getCardExpiryYear()
+    {
+        return cardExpiryYear;
+    }
+    public User getUser() { return user;}
+
+    //SETTERS
+    public void setCardNumber(String cardNumber)
+    {
+        this.cardNumber = cardNumber;
+    }
+    public void setUser (User user) { this.user = user; }
+    public void setCardSecurityPin(int cardSecurityPin)
+    {
+        this.cardSecurityPin = cardSecurityPin;
+    }
+    public void setCardExpiryMonth(int cardExpiryMonth)
+    {
+        this.cardExpiryMonth = cardExpiryMonth;
+    }
+    public void setCardExpiryYear(int cardExpiryYear)
+    {
+        this.cardExpiryYear = cardExpiryYear;
+    }
+}
