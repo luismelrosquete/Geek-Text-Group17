3ea79d4e9852c7a88363a25fde316a5763ab8cf1
robulonn <25264923+robulonn@users.noreply.github.com>
2021-10-04 10:55:08 -0400

    small changes
    added QoL changes for output of findByBooks and findByGenre (better formatting).
    made it so only a few fields are required for adding a new book (only isbn, name, and genre) and the rest are optional fields (added curls in comments).
    changed path mapping for findbyisbn to camel casing for consistency (findByIsbn).

--- src/main/java/com/example/geektext/BookController.java
+++ src/main/java/com/example/geektext/BookController.java
@@ -12,21 +12,31 @@ public class BookController
     @Autowired
     private BookRepository bookRepository;
 
-    //curl test //curl localhost:8080/book/addBook -d name=book4 -d genre=genre2 -d price=15
+    //curl test //curl localhost:8080/book/addBook -d bookIsbn=5780 -d name=book4 -d genre=Genre2
+    //curl test //curl localhost:8080/book/addBook -d bookIsbn=5780 -d name=book4 -d genre=Genre2 -d description=testDesc -d publisher=testPublisher -d yearPublished=2020 -d copiesSold=15 -d price=15
     @PostMapping(path = "/addBook")    //Map *only* POST requests
     public @ResponseBody
-    String addBook (@RequestParam String bookIsbn, @RequestParam String name, @RequestParam String description, @RequestParam String publisher, @RequestParam String genre,
-                    @RequestParam Integer yearPublished, @RequestParam Integer copiesSold, @RequestParam Integer price)
-    { // save all of the book's information
-        Book book = new Book(); 
+    String addBook (@RequestParam String bookIsbn, @RequestParam String name, @RequestParam String genre,
+                    @RequestParam(required = false) String description, @RequestParam(required = false) String publisher,
+                    @RequestParam(required = false) Integer yearPublished, @RequestParam(required = false) Integer copiesSold,
+                    @RequestParam(required = false) Integer price)
+    {
+        // save all of the book's information
+        Book book = new Book();
+
+        //required fields
         book.setBookIsbn(bookIsbn);
         book.setBookName(name);
-        book.setBookDescription(description);
-        book.setBookPublisher(publisher);
         book.setBook_genre(genre);
-        book.setBookYearPublished(yearPublished);
-        book.setBookCopiesSold(copiesSold);
-        book.setBookPrice(price);
+
+        //optional fields
+        if (description != null) book.setBookDescription(description);
+        if (publisher != null) book.setBookPublisher(publisher);
+        if (yearPublished != null) book.setBookYearPublished(yearPublished);
+        if (copiesSold != null) book.setBookCopiesSold(copiesSold);
+        if (price != null) book.setBookPrice(price);
+
+        //save book
         bookRepository.save(book);
         return "Saved book";
     }
@@ -40,12 +50,12 @@ public class BookController
         //testing output
         String out = "";
         for (int i = 0; i < books.stream().count(); i++)
-            out += books.get(i).toString() + ((i+1 != books.stream().count()) ? " | " : "");
+            out += books.get(i).toString() + ((i+1 != books.stream().count()) ? "\n" : "");
         return out;
     }
 
-    //curl test  //curl localhost:8080/book/findbyisbn -d isbn=4987
-    @PostMapping (path = "/findbyisbn")
+    //curl test  //curl localhost:8080/book/findByIsbn -d isbn=5780
+    @PostMapping (path = "/findByIsbn")
     public @ResponseBody String book_findbyisbn (@RequestParam String isbn)
     {
         List<Book> books = bookRepository.findBybookIsbn (isbn);
@@ -53,13 +63,12 @@ public class BookController
         //testing output
         String out = "";
         for (int i = 0; i < books.stream().count(); i++)
-            out += books.get(i).toString() + ((i+1 != books.stream().count()) ? " | " : "");
+            out += books.get(i).toString() + ((i+1 != books.stream().count()) ? "\n" : "");
         return out;
     }
   
     //curl test //curl localhost:8080/book/allBooks
     @GetMapping(path = "/allBooks")
-
     public @ResponseBody Iterable<Book> getAllBooks ()
     {
         return bookRepository.findAll();
