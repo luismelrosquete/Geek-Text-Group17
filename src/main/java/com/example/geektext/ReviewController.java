efafc5be9228b5c3c1f62e5e1f53d7bd1a47e145
Oca119 <90145931+Oca119@users.noreply.github.com>
2021-10-22 17:00:58 -0400

    update

---
+++ src/main/java/com/example/geektext/ReviewController.java
@@ -0,0 +1,43 @@
+package com.example.geektext;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewsController {
+    @Autowired
+    private ReviewsRepository reviewsRepository;
+    @Autowired
+    private BookRepository bookRepository;
+    public void addReview(Integer reviewID, Integer bookCode, Integer userID, Double rating, String comment, Date datestamp) {
+        reviewsRepository.insertReview(reviewID, bookCode, comment, datestamp, rating, userID);
+        this.updateAvgRating(bookCode, this.findAvg(bookCode));
+    }
+
+    public void updateAvgRating(Integer bookCode, Double rating) {
+        Book books = bookRepository.findBybookIsbn(bookCode);
+        books.
+        bookRepository.save(books);
+
+    }
+
+    public Double findAvg(Integer bookCode) {
+        return reviewsRepository.pullAvg(bookCode).get(0);
+    }
+
+    public List<Reviews> getAllReviews() {
+        List<Reviews> reviews = new ArrayList<>();
+        reviewsRepository.findByOrderByRatingDesc().forEach(reviews::add);
+
+
+        return reviews;
+    }
+
+}
---
+++ src/main/java/com/example/geektext/ReviewsRepository.java
@@ -0,0 +1,30 @@
+package com.example.geektext;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface ReviewsRepository extends JpaRepository<Reviews, Integer> {
+
+    List<Reviews> findByOrderByRatingDesc();
+
+    @Transactional
+    @Query(value = "INSERT INTO reviews (reviewid, book_code, comment, datestamp, rating, userid)"
+            + "VALUES (:reviewid, :book_code, :comment, :datestamp, :rating, :userid)", nativeQuery = true)
+    @Modifying
+    void insertReview(@Param("reviewid") Integer reviewid, @Param("book_code") Integer book_code, @Param("comment")
+            String comment, @Param("datestamp") Date datestamp, @Param("rating") Double rating, @Param("userid") Integer userID);
+
+    @Transactional
+    @Query(value ="SELECT AVG(rating) FROM reviews WHERE reviews.book_code = (:book_code)", nativeQuery = true)
+    List<Double> pullAvg(@Param("book_code") Integer book_code);
+
+
+
+
+}
