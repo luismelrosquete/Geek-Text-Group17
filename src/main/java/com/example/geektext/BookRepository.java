77b9f6319a5d596ee481d2783396d0cd58697a0b
luismelrosquete <75111843+luismelrosquete@users.noreply.github.com>
2021-10-28 21:09:22 -0400

    Update BookRepository.java
    Find book by author

--- src/main/java/com/example/geektext/BookRepository.java
+++ src/main/java/com/example/geektext/BookRepository.java
@@ -13,6 +13,9 @@ public interface BookRepository extends PagingAndSortingRepository<Book, String>
     // finding book by ISBN
     List<Book> findBybookIsbn (String bookIsbn);
 
+    // finding book by author
+    List<Book> findByBookAuthor (String bookAuthors);
+
     //finding top sellers (top 10 books that have most copies sold)
     List<Book> findTop10ByOrderByBookCopiesSoldDesc ();
 
